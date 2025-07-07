3302bb2632b2d2c2020d065c16d558c03098f2ea
user <user@WIN-M1JADIG1GJ0>
2025-07-07 18:15:29 +0300

    Fraction day 1

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -67,6 +67,20 @@ public:
 		return *this;
 	}
 
+	Point& operator++()    //Prefix increment
+	{
+		x++;
+		y++;
+		return *this;
+	}
+	Point operator++(int)  //Postfix (Suffix) increment
+	{
+		Point old = *this; // сохраняем старое значение объекта
+		x++;
+		y++;
+		return old;
+	}
+
 	//        Methods:
 	double distance(Point& other)const
 	{
@@ -95,11 +109,33 @@ public:
 		return distance;
 	}
 
+	Point operator+(const Point& left, const Point& right)
+	{
+		Point result;
+		result.set_x(left.get_x() + right.get_x());
+		result.set_y(left.get_y() + right.get_y());
+		return result;
+	}
 
+	bool operator== (const Point& left, const Point& right)
+	{
+		/* 
+		if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+			return true;
+		else
+			return false;
+		*/
+		return left.get_x() == right.get_x() && left.get_y() == right.get_y();
+	}
+	bool operator!=(const Point& left, const Point& right)
+	{
+		return !(left == right);
+	}
 //#define STRUCT_POINT
 //#define DINSTANCE_CHEK
 //#define CONSTRUCTORS_CHECK
-
+//#define ASSIGNMENT_CHECK
+//#define ARIFMETICAL_OPERATORS_CHECK
 
 void main()
 {
@@ -175,6 +211,7 @@ void main()
 
 #endif // CONSTRUCTORS_CHECK
 
+#ifdef ASSIGNMENT_CHECK
 	int a, b, c;
 	a = b = c = 0;
 	cout << a << "\t" << b << "\t" << c << endl;
@@ -186,4 +223,20 @@ void main()
 	A.print();
 	B.print();
 	C.print();
+#endif // ASSIGNMENT_CHECK
+
+#ifdef ARIFMETICAL_OPERATORS_CHECK
+	Point A(2, 3);
+	Point B(7, 8);
+	Point C = A + B;
+	C.print();
+
+	A = B++;
+	A.print();
+	B.print();
+#endif // ARIFMETICAL_OPERATORS_CHECK
+
+	cout << (Point(2, 3) != Point(7, 8)) << endl;
+
+
 }
