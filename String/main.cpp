c7848c06dd20ce3ead95d79c4cebcff7d1aabd27
user <user@WIN-M1JADIG1GJ0>
2025-08-13 22:33:06 +0300

    Calling constructors DONE

--- String/main.cpp
+++ String/main.cpp
@@ -130,7 +130,7 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 	return os << obj.get_str();
 }
 
-#define CONSTRUCTORS_CHECK
+//#define CONSTRUCTORS_CHECK
 //#define COPY_SEMANTIC_CHECK
 
 void main()
@@ -170,6 +170,41 @@ void main()
 
 #endif // COPY_SEMANTIC_CHECK
 
+	String str1;    //DefaultConstructor
+	str1.print();
 
+	String str2(5);//Single-Argument Constructor (int)
+	str2.print();
 
+	String str3 = "Hello";    //Singl-argument Constructor (const char str[])
+	str3.print();
+
+	String str4();     //В этой строке не вызывается DefaultConstructor, и не создается объект,
+	//здесь объявляется функция 'str4', которая ничего не принимает,
+	//и возвращает значение типа 'String'.
+//str4 НЕ является объектом.
+
+//то есть, круглые скобки не делают явный вызов конструктора по умолчанию,
+//если есть необходимость явно вызвать конструктор по умолчанию, 
+//то это можно сделать при помощи фигурных скобок:
+
+	String str5{};   //А это строка уже явно вызывает DefaultConstructor()
+	str5.print();
+
+	String str6{ 6 }; //Singl-Argument Constructor (int)
+	str6.print();
+
+	String str7{ "World" };
+	str7.print();
+
+	String str8 = str7;
+	str8.print();
+
+	String str9(str8);
+	str9.print();
+
+	String str10{ str9 };
+	str10.print();
+
+	//Фигурные скобки для вызова конструкторов следует использовать с большой осторожностью.
 }
