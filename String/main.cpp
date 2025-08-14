909c787174af6fd08638628a8b651d8bb44542e8
user <user@WIN-M1JADIG1GJ0>
2025-08-14 22:37:43 +0300

    HeaderInitialozation In String DONE

--- String/main.cpp
+++ String/main.cpp
@@ -22,37 +22,37 @@ public:
 	}
 
 	//     constructors:
-	explicit String(int size = 80)
+	explicit String(int size = 80):size(size), str(new char[size]{})
 	{
 		//конструктор по умолчанию создает пустую строку размером 80 Байт
-		this->size = size;
-		this->str = new char[size] {};
+		//this->size = size;
+		//this->str = new char[size] {};
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
-	String(const char* str)
+	String(const char* str):size(strlen(str)+1), str(new char[size]{})
 	{
-		this->size = strlen(str) + 1;   //strlen() возвращает размер строки в символах, +1 нужен чтобы выделилась память под NULL-terminators
-		this->str = new char[size] {}; 
+		//this->size = strlen(str) + 1;   //strlen() возвращает размер строки в символах, +1 нужен чтобы выделилась память под NULL-terminators
+		//this->str = new char[size] {}; 
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other):size(other.size), str(new char[size]{})
 	{
 		//this->str = other.str;     //Shallow Copy
 		/// ------------------------------- ///
 		//Deep Copy:
-		this->size = other.size;
-		this->str = new char[size] {};
+		//this->size = other.size;
+		//this->str = new char[size] {};
 		for (int i = 0; i < size; i++)
 			this->str[i] = other.str[i];
 
 		cout << "CopyConstructor:\t" << this << endl;
 	}
-	String(String&& other)
+	String(String&& other):size(other.size),str(other.str)
 	{
 		//MoveConstructor - ShallowCopy
-		this->size = other.size;
-		this->str = other.str;
+		//this->str = other.str;
+		//this->size = other.size;
 		//Обнуляем принимаемый объект для того чтобы предотвратить удаление его ресурсов деструктором.
 		other.size = 0;
 		other.str = nullptr;
@@ -207,4 +207,7 @@ void main()
 	str10.print();
 
 	//Фигурные скобки для вызова конструкторов следует использовать с большой осторожностью.
+
+	String str11 = str3 + str8;
+	str11.print();
 }
